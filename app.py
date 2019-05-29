476d2e21accf4d47c461a4d7ebaab16b6958bbee
howeverforever <longbiau@hotmail.com.tw>
2019-05-29 16:05:35 +0800

    Configure independently to source type.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 import tempfile
+import hashlib
 
 from config import Config
 from util.transcription import transcribe
 
+from boto3.dynamodb.conditions import Key, Attr
 from flask import Flask, request, abort, send_from_directory
 from linebot.exceptions import LineBotApiError, InvalidSignatureError
 from linebot.models import *
@@ -38,6 +40,8 @@ def handle_text_message(event):
     print(event)
     msg = event.message.text
     msg = msg.lower()
+
+    is_on = None
     if msg == 'help':
         line_bot_api.reply_message(event.reply_token,
                                    TextSendMessage(text='1. Enter \"on\" to turn transcription on.\n'
@@ -46,10 +50,83 @@ def handle_text_message(event):
         config.transcription_mode = True
         line_bot_api.reply_message(event.reply_token,
                                    TextSendMessage(text='>> Transcription Mode is turned ON. <<'))
+        is_on = True
     elif msg == 'off':
         config.transcription_mode = False
         line_bot_api.reply_message(event.reply_token,
                                    TextSendMessage(text='>> Transcription Mode is turned OFF. <<'))
+        is_on = False
+
+    if is_on is not None:
+        sha256_id = None
+        if event.source.type == 'user':
+            user_id = event.source.user_id
+            sha256_id = hashlib.sha256(user_id.encode()).hexdigest()
+        elif event.source.type == 'room':
+            room_id = event.source.room_id
+            sha256_id = hashlib.sha256(room_id.encode()).hexdigest()
+        elif event.source.type == 'group':
+            group_id = event.source.group_id
+            sha256_id = hashlib.sha256(group_id.encode()).hexdigest()
+
+        if sha256_id:
+            config.table.update_item(
+                Key={
+                    'id': sha256_id,
+                },
+                UpdateExpression='SET is_on = :is_on',
+                ExpressionAttributeValues={
+                    ':is_on': is_on
+                }
+            )
+
+
+@handler.add(JoinEvent)
+def handle_join_event(event):
+    print(event)
+
+    sha256_id = None
+    if event.source.type == 'user':
+        user_id = event.source.user_id
+        sha256_id = hashlib.sha256(user_id.encode()).hexdigest()
+    elif event.source.type == 'room':
+        room_id = event.source.room_id
+        sha256_id = hashlib.sha256(room_id.encode()).hexdigest()
+    elif event.source.type == 'group':
+        group_id = event.source.group_id
+        sha256_id = hashlib.sha256(group_id.encode()).hexdigest()
+
+    if sha256_id is not None:
+        config.table.put_item(
+            Item={
+                'id': sha256_id,
+                'type': event.source.type,
+                'is_on': True
+            }
+        )
+
+
+@handler.add(LeaveEvent)
+def handle_leave_event(event):
+    print(event)
+
+    sha256_id = None
+    if event.source.type == 'user':
+        user_id = event.source.user_id
+        sha256_id = hashlib.sha256(user_id.encode()).hexdigest()
+    elif event.source.type == 'room':
+        room_id = event.source.room_id
+        sha256_id = hashlib.sha256(room_id.encode()).hexdigest()
+    elif event.source.type == 'group':
+        group_id = event.source.group_id
+        sha256_id = hashlib.sha256(group_id.encode()).hexdigest()
+
+    if sha256_id is not None:
+        config.table.delete_item(
+            Key={
+                'id': sha256_id
+            }
+        )
 
 
 @handler.add(MessageEvent, message=AudioMessage)
@@ -57,12 +134,29 @@ def handle_audio_message(event):
     print(event)
 
     reply_messages = []
-    message_content = line_bot_api.get_message_content(event.message.id)
-    with tempfile.NamedTemporaryFile(dir=config.static_tmp_path, suffix='.m4a', delete=False) as fp:
-        for chunk in message_content.iter_content():
-            fp.write(chunk)
 
-    if config.transcription_mode:
+    sha256_id = None
+    if event.source.type == 'user':
+        user_id = event.source.user_id
+        sha256_id = hashlib.sha256(user_id.encode()).hexdigest()
+    elif event.source.type == 'room':
+        room_id = event.source.room_id
+        sha256_id = hashlib.sha256(room_id.encode()).hexdigest()
+    elif event.source.type == 'group':
+        group_id = event.source.group_id
+        sha256_id = hashlib.sha256(group_id.encode()).hexdigest()
+
+    response = config.table.query(
+        KeyConditionExpression=Key('id').eq(sha256_id)
+    )
+
+    print(response)
+    if sha256_id is not None and response['Count'] > 0 and response['Items'][0]['is_on']:
+        message_content = line_bot_api.get_message_content(event.message.id)
+        with tempfile.NamedTemporaryFile(dir=config.static_tmp_path, suffix='.m4a', delete=False) as fp:
+            for chunk in message_content.iter_content():
+                fp.write(chunk)
+
         text = transcribe(fp.name)
         os.remove(fp.name)
 
